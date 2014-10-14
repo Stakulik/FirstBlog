e25fa615b4700961da8d07bd38a78f2729360339
Stakulik <stakulik@mail.ru>
2014-10-14 22:16:57 +0400

    Add sign in & sign out functions

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,26 @@
 class User < ActiveRecord::Base
- has_many :articles, dependent: :destroy
- before_save { self.email = email.downcase }
- before_save { self.name = name.downcase }
- validates :name, presence: true, length: {minimum: 5, maximum: 15}, uniqueness: { case_sensitive: false }
- VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+[\w]+\.[a-z]+\z/i  #сравнить с тем, что в учебнике - 6 гл.
- validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
- has_secure_password
- validates :password, length: { minimum: 6 }
+  has_many :articles, dependent: :destroy
+  before_save { self.email = email.downcase }
+  before_create :create_remember_token
+  before_save { self.name = name.downcase }
+  validates :name, presence: true, length: {minimum: 5, maximum: 15}, uniqueness: { case_sensitive: false }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+[\w]+\.[a-z]+\z/i  #сравнить с тем, что в учебнике - 6 гл.
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+  has_secure_password
+  validates :password, length: { minimum: 6 }
+
+  def User.new_remember_token
+  	SecureRandom.urlsafe_base64
+  end
+
+  def User.encrypt(token)
+  	Digest::SHA1.hexdigest(token.to_s)
+  end
+
+  private
+
+    def create_remember_token
+    	self.remember_token=User.encrypt(User.new_remember_token)
+    end
+    
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 FirstBlog::Application.routes.draw do
 
+  resources :users
+  resources :sessions, only: [:new, :create, :destroy]
   root 'static_pages#home'
   match '/articles', to: 'static_pages#articles', via: 'get'
   match '/about', to: 'static_pages#about', via: 'get'
   match '/signup', to: 'users#new', via: 'get'
-  resources :users
+  match '/signin', to: 'sessions#new', via: 'get'
+  match '/signout', to: 'sessions#destroy', via: 'delete'
   resources :articles
 
   # The priority is based upon order of creation: first created -> highest priority.
