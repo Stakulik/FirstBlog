fe823a6533119bcf606af05666c99cb215155e7a
Stakulik <stakulik@mail.ru>
2015-01-30 13:44:20 +0300

    Add h1 to the main page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,7 +10,7 @@ class SessionsController < ApplicationController
     	sign_in user
     	redirect_to root_path
     else
-    	flash.now[:danger] = "Неправильные данные"
+    	flash.now[:danger] = "Неправильные логин / пароль"
     	render 'new'
     end
   end
