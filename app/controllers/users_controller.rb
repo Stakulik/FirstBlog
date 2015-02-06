242bd85839906599cf080442ac249795df4e126f
Stakulik <stakulik@mail.ru>
2015-02-06 14:34:28 +0300

    Fix creating new user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,8 +16,8 @@ class UsersController < ApplicationController
   def create
   	@user = User.new(user_params)
   	if @user.save
-      sign_in @user
-      flash[:success] = "Добро пожаловать!"
+  #    sign_in @user
+      flash[:success] = "Пользователь #{@user.name} создан"
       redirect_to @user
   	else
   	  render 'new'
